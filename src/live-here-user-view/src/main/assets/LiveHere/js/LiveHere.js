b06811fb67ddfedf0e7114ed8d89d9abbe4e6a19
shibinthomas <shibin@hexagonglobal.in>
2015-04-19 11:43:50 +0530

    js

--- src/live-here-user-view/src/main/assets/LiveHere/js/LiveHere.js
+++ src/live-here-user-view/src/main/assets/LiveHere/js/LiveHere.js
@@ -1,1 +1,10 @@
 LiveHere = new Backbone.Marionette.Application();
+
+
+LiveHere.on('start',function(){
+
+    if(Backbone.history){
+        Backbone.history.start();
+    }
+});
+LiveHere.start();
